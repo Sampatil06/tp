95413bf93f2a7e4aef13c23c5e6672345e381d0c
Sampatil06 <113002662+Sampatil06@users.noreply.github.com>
2023-10-19 21:51:42 +0530

    j

--- ip_project/settings.py
+++ ip_project/settings.py
@@ -14,9 +14,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-import dj_database
+import dj_database_url
 from decouple import config
-
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
